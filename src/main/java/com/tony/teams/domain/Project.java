a30ec71956a4767abcb4f9ba63f0da63eb90d714
Tony Park <tonypark0403@gmail.com>
2020-03-11 22:45:38 -0400

    Added validation from server to client

--- src/main/java/com/tony/teams/domain/Project.java
+++ src/main/java/com/tony/teams/domain/Project.java
@@ -1,9 +1,13 @@
 package com.tony.teams.domain;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.*;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 @Entity
 @NoArgsConstructor
@@ -14,11 +18,18 @@ public class Project {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @NotBlank(message = "Project name is required")
     private String projectName;
+    @NotBlank(message = "Project identifier is required")
+    @Size(min = 4, max = 5, message = "Please use 4 to 5 characters")
+    @Column(updatable = false, unique = true)
     private String projectIdentifier;
+    @NotBlank(message = "Project description is required")
     private String description;
-    private LocalDate startDate;
-    private LocalDate endDate;
+    @JsonFormat(pattern = "yyyy-mm-dd")
+    private LocalDateTime startDate;
+    @JsonFormat(pattern = "yyyy-mm-dd")
+    private LocalDateTime endDate;
 
     private LocalDate createdAt;
     private LocalDate updatedAt;
--- src/main/java/com/tony/teams/web/ProjectController.java
+++ src/main/java/com/tony/teams/web/ProjectController.java
@@ -5,7 +5,13 @@ import com.tony.teams.services.ProjectService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/project")
@@ -20,8 +26,16 @@ public class ProjectController {
     }
 
     @PostMapping
-    public ResponseEntity<Project> createNewProject(Project project){
+    public ResponseEntity<?> createNewProject(@Valid @RequestBody Project project, BindingResult result){
         System.out.println(project);
+        if(result.hasErrors()){
+            Map<String, String> errorMap = new HashMap<>();
+            for(FieldError error : result.getFieldErrors()) {
+                errorMap.put(error.getField(), error.getDefaultMessage());
+            }
+            return new ResponseEntity<>(errorMap, HttpStatus.BAD_REQUEST);
+        }
+
         project = projectService.saveOrUpdateProject(project);
         return new ResponseEntity<>(project, HttpStatus.CREATED);
     }
